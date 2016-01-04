56ee5fdb9b135740ec7de62db32760d9c14e91eb
iamquang95 <iamquang95@gmail.com>
2016-01-04 21:12:17 +0700

    Fix bug empty JSON

--- AlbumMP3ZingDownloader/AlbumMP3ZingDownloader/spiders/song_spider.py
+++ AlbumMP3ZingDownloader/AlbumMP3ZingDownloader/spiders/song_spider.py
@@ -5,12 +5,19 @@ from AlbumMP3ZingDownloader.items import SongItem
 
 
 class SongSpider(scrapy.Spider):
+    # Const value
+    HOST = "mp3.zing.vn"
+    TAB_CONTAIN_XML = 'div[@id="html5player"]/@data-xml'
+
     name = "song"
-    allowed_domains = ["mp3.zing.vn"]
+    allowed_domains = [HOST]
     # Create a list of song link based on JSON file created by AlbumCrawler
     # Uncomment after crawl album
     with open("ListSong.json") as list_song_file:
-        list_song = json.load(list_song_file)
+        try:
+            list_song = json.load(list_song_file)
+        except ValueError:
+            list_song = []
     start_urls = []
     for i in xrange(0, len(list_song)):
         print list_song[i]['link'][0]
@@ -19,5 +26,5 @@ class SongSpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//div[@id="_player"]'):
             item = SongItem()
-            item['link'] = sel.xpath('div[@id="html5player"]/@data-xml').extract()
+            item['link'] = sel.xpath(SongSpider.TAB_CONTAIN_XML).extract()
             yield item
